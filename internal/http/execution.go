1612a442359120c4a091f2491f81717ad574189b
Pete Smith <pete.smith.ext@flomation.co>
2026-05-19 23:11:36 +0100

    Update company address to Ruscoe House, Whitchurch

--- internal/http/execution.go
+++ internal/http/execution.go
@@ -706,7 +706,7 @@ const notificationEmailTemplate = `<!DOCTYPE html>
                             <tr>
                                 <td style="font-size: 10px; color: #9ca3af; line-height: 1.5;">
                                     <strong>Flomation Ltd</strong><br>
-                                    33-34 High Street, Bridgnorth, Shropshire, England, WV16 4DB<br/><br/>
+                                    Ruscoe House, The Chequer, Whitchurch, Wrexham, Wales, SY13 2JJ<br/><br/>
                                     This is an automated notification from the Flomation workflow platform.<br/><br/>
                                     Execution ID: {{.TransactionID}}<br/>
                                     Sent: {{.TransactionTime}}
